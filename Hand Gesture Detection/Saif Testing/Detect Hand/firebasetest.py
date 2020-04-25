cf998f66c668c7d4b33e7ea0ffc5668ab9749d30
rhridwan <ridwanul.haque@northsouth.edu>
2020-04-25 11:25:26 +0600

    nothing

--- Hand Gesture Detection/Saif Testing/Detect Hand/firebasetest.py
+++
@@ -1,5 +0,0 @@
-from firebase import firebase
-firebase = firebase.FirebaseApplication('https://assist-5d80b.firebaseio.com/', None)
-
-firebase.put('/Status/-M2zMzoxqItNfwVJ3tUY','STATUS','E')
-print('Record Updated')
